e775226dd963e45ad5616593ea99fa61b101e353
Zoey Chesny <z.chesny@gmail.com>
2020-04-15 01:48:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,6 +22,7 @@ class Transfer
         self.receiver.balance += amount
         self.status = "complete"
       else
+        self.status = "rejected"
         "Transaction rejected. Please check your account balance."
       end
     end
