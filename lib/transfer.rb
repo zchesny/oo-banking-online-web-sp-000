da7eb9243fb076535296c87d6dc275df872b6a08
Zoey Chesny <z.chesny@gmail.com>
2020-04-15 01:39:37 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 class Transfer
+  attr_accessor :sender, :receiver, :amount, :status
+
   # your code here
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+    if self.status != "complete"
+      self.sender.balance -= amount
+      self.receiver.balance += amount
+      self.status = "complete"
+    end
+  end
+
+  def reverse_transfer
+  end
+
 end
